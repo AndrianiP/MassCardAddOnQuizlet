77d4904303ff0fa3b3241d6cda008e87e25683b4
Andriani <42726095+AndrianiP@users.noreply.github.com>
2022-07-31 01:19:26 -0400

    Update AutoMassCardAdd.js

--- AutoMassCardAdd.js
+++ AutoMassCardAdd.js
@@ -7,6 +7,7 @@
 // @match        https://quizlet.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=quizlet.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 window.onload = function(){
